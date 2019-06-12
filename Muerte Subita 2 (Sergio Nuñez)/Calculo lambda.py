2c98aaceed465cdd4535b376a2d6dffef0c8c8f2
Gustavo Garcia <31778638+GustavoGarcia05@users.noreply.github.com>
2019-06-12 13:20:24 -0500

    Update Calculo lambda.py

--- Muerte Subita 2 (Sergio Nuñez)/Calculo lambda.py
+++ Muerte Subita 2 (Sergio Nuñez)/Calculo lambda.py
@@ -1,3 +1,5 @@
 #Dada una lista de tuplas caracterizada por (int x, int y) sumar los x que cumplan con el criterio de ser el número triangular de y
 import functools
-print(list(filter(lambda x: x[0] == ((x[1]*(x[1]+1))/2), [[1,1], [5,2], [8,3], [10,4], [23,5], [31,6], [28,7], [36,8], [47,9], [55,10], [77,11], [93,12]])))
+from functools import reduce
+lista= [[1,1], [5,2], [8,3], [10,4], [23,5], [31,6], [28,7], [36,8], [47,9], [55,10], [77,11], [93,12]]
+print(reduce(lambda x,y: x+y, list(map(lambda x:x[1], list(filter(lambda x: x[0] == ((x[1]*(x[1]+1))/2),lista)) )) ))

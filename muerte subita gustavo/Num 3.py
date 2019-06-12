b58cd96323b95b4f29a2168462c8ce5db3badeac
gustavo garcia <dannyrr05@gmail.com>
2019-06-12 03:22:32 -0500

    1,3,5,6 funcionan

--- muerte subita gustavo/Num 3.py
+++ muerte subita gustavo/Num 3.py
@@ -5,16 +5,4 @@ from functools import reduce
 def retornar2():
     return [[1,2,3,4],[7,6,7,8],[5,6],[1],[0,9,8,7,6,5]]
 
-def retornar():
-    return [1,2,3,4,5,6]
-
-def valormax(lista):
-    
-    if lista==[]:
-        return 0
-    if lista[0]>lista[1]:
-        return valormax(lista[1:]) 
-    
-    
-
-print(valormax(retornar()))
+print(list(map(lambda x:max(x),retornar2())))
